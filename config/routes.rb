29a514d532712f70fb264c877d27cd33c8496a61
jaredkotoff <krylic@gmail.com>
2015-04-19 23:13:14 +0000

    renamed home page to index

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,9 @@
 Rails.application.routes.draw do
-  get 'pages/home'
-
-  get 'pages/statistics'
-
-  get 'pages/help'
-
-  get 'pages/about'
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'pages#home'
+  root 'pages#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
