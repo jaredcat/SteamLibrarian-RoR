3832b2d8ad652163eb82c1628d7fa16b8f27cf48
AlbertoGo <otrebla117@yahoo.com>
2015-04-20 03:13:24 +0000

    added newGame, updateGame, and deleteGame to game.rb

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,4 +3,35 @@ class Game < ActiveRecord::Base
   has_many :game_themes
   has_many :game_genres
   has_many :users_games
+  
+  def self.newGame(gameObject, steamID)
+    #create new Game entry
+    game = Game.new
+    game.appid = steamID
+    game.gb_id = gameObject['id']
+    game.name = gameObject['name']
+    game.api_detail_url = gameObject['api_detail_url']
+    game.dec = gameObject['deck']
+    game.image = gameObject['image']
+    game.date_last_updated = gameObject['date_last_updated']
+    game.save
+  end
+  
+  def self.updateGame(gameObject)
+    #update existing entry
+    game = Game.find_by(gb_id: gameObject['id'])
+    game.name = gameObject['name']
+    game.api_detail_url = gameObject['api_detail_url']
+    game.dec = gameObject['deck']
+    game.image = gameObject['image']
+    game.date_last_updated = gameObject['date_last_updated']
+    game.save
+  end
+  
+  def self.deleteGame(gameID)
+    #delete existing game object
+    game = Game.find_by(id: gameID)
+    game.destroy
+  end
+  
 end
